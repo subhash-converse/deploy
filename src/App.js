9796241835f7445c658382a695702f0c2933535a
subhash <subhash@conversedatasolutions.com>
2024-11-01 15:05:31 +0530

    ssds

--- src/App.js
+++ src/App.js
@@ -4,10 +4,9 @@ function App() {
   return (
     <div className="App">
       <h1>யோவ் மனங்கெட்டா சாக்லேட் ....</h1>
-      <h1>ஒக்காளி நீ படிச்ச school naa <span style={{color:"red"}}>head master</span>....</h1>
+      <h1>ஒக்காளி நீ படிச்ச  school naa <span style={{color:"red"}}>head master</span>....</h1>
     </div>
   );
 }
 
 export default App;
-  
